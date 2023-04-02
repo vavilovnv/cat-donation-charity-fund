4ff87f027790ff6b325d43a15540f68d4d5bb196
nike <vavilovnv@gmail.com>
2023-04-02 22:14:39 +0300

    App structure created

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from pydantic import BaseSettings, EmailStr
+
+
+class Settings(BaseSettings):
+    app_title: str = 'API QRKot'
+    app_desc: str = 'Сервис учета пожертвований на котиков'
+    database_url: str
+    secret: str
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
+
+    class Config:
+        env_file: '.env'
+
+
+settings = Settings()
