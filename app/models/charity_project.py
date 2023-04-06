504ccc265625f074d7837a55b3036df83a5804dd
nike <vavilovnv@gmail.com>
2023-04-06 19:47:03 +0300

    Models added

---
+++ app/models/charity_project.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, String, Text
+
+from app.models.abstract_model import AbstractModel
+
+
+class CharityProject(AbstractModel):
+    name = Column(String(100), nullable=False, unique=True)
+    description = Column(Text, nullable=False)
---
+++ app/models/donation.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, ForeignKey, Integer, Text
+
+from app.models.abstract_model import AbstractModel
+
+
+class Donation(AbstractModel):
+    comment = Column(Text, nullable=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
