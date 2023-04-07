34c6be8420f46fc511998b6649e38357a0ac0b3d
nike <vavilovnv@gmail.com>
2023-04-07 20:04:39 +0300

    Schemas updated

--- app/schemas/charity_project.py
+++ app/schemas/charity_project.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel, Extra, Field
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Extra, Field, validator
 
 
 class CharityProjectUpdate(BaseModel):
@@ -14,3 +17,20 @@ class CharityProjectCreate(CharityProjectUpdate):
     name: str = Field(..., min_length=1, max_length=100)
     description: str = Field(..., min_length=1)
     full_amount: int = Field(..., gt=0)
+
+    @validator('name', 'description')
+    def none_and_empty_not_allowed(cls, value: str):
+        if not value or value is None:
+            raise ValueError('Все поля обязательны для заполнения.')
+        return value
+
+
+class CharityProjectDB(CharityProjectCreate):
+    id: int
+    invested_amount: int
+    fully_invested: bool
+    create_date: datetime
+    close_date: Optional[datetime]
+
+    class Config:
+        orm_mode = True
--- app/schemas/donation.py
+++ app/schemas/donation.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel, Extra, Field
@@ -9,3 +10,18 @@ class DonationCreate(BaseModel):
 
     class Config:
         extra = Extra.forbid
+
+
+class DonationView(DonationCreate):
+    id: int
+    create_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class DonationDB(DonationView):
+    user_id: int
+    invested_amount: int
+    fully_invested: bool
+    close_date: Optional[datetime]
