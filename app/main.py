b4ad1012e0c830b081f3fc6902f22593e815071a
nike <vavilovnv@gmail.com>
2023-04-08 16:23:38 +0300

    init db functions added

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
+from app.core.init_db import create_first_superuser
 
 app = FastAPI(
     title=settings.app_title,
@@ -13,5 +14,6 @@ app = FastAPI(
 app.include_router(main_router)
 
 
-if __name__ == '__main__':
-    uvicorn.run('app.main:app', reload=True)
+@app.on_event('startup')
+async def startup():
+    await create_first_superuser()
