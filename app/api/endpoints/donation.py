5f8435fa0514b1cf9bb2df5dabff0b8767745728
nike <vavilovnv@gmail.com>
2023-04-08 16:26:36 +0300

    feat: Donation endpoint extended

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -3,9 +3,13 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.utils import investing_process
 from app.core.db import get_async_session
+from app.core.user import current_user
 from app.crud.donation import donation_crud
-from app.schemas.donation import DonationDB
+from app.models.user import User
+from app.schemas.donation import DonationCreate, DonationDB, DonationView
+
 
 router = APIRouter()
 
@@ -19,3 +23,30 @@ async def get_all_donations(
     session: AsyncSession = Depends(get_async_session)
 ):
     return await donation_crud.get_multi(session)
+
+
+@router.get(
+    '/my',
+    response_model=List[DonationView],
+    response_model_exclude_none=True
+)
+async def get_user_donations(
+    user: User = Depends(current_user),
+    session: AsyncSession = Depends(get_async_session)
+):
+    return await donation_crud.get_user_donations(user, session)
+
+
+@router.post(
+    '/',
+    response_model=DonationView,
+    response_model_exclude_none=True
+)
+async def create_donation(
+    donation: DonationCreate,
+    user: User = Depends(current_user),
+    session: AsyncSession = Depends(get_async_session)
+):
+    donation = await donation_crud.create(donation, session, user)
+    donation = await investing_process(session, donation)
+    return donation
