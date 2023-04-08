720d2be59031720294ae04c1adadf09bc8207a94
nike <vavilovnv@gmail.com>
2023-04-08 16:22:43 +0300

    feat: endpoint charity_project extended

--- app/api/endpoints/charity_project.py
+++ app/api/endpoints/charity_project.py
@@ -3,10 +3,14 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.validators import check_name_duplicate
+from app.api.validators import (
+    check_project_name_is_exist, check_project_is_closed,
+    check_project_is_exist, check_money_amount
+)
+from app.api.utils import investing_process
 from app.core.db import get_async_session
 from app.crud.charity_project import charity_project_crud
-from app.schemas.charity_project import CharityProjectCreate, CharityProjectDB
+from app.schemas.charity_project import CharityProjectCreate, CharityProjectDB, CharityProjectUpdate
 
 router = APIRouter()
 
@@ -14,7 +18,7 @@ router = APIRouter()
 @router.get(
     '/',
     response_model=List[CharityProjectDB],
-    response_model_exclude_none=True
+    response_model_exclude_none=True,
 )
 async def get_all_charity_projects(
     session: AsyncSession = Depends(get_async_session)
@@ -31,6 +35,46 @@ async def create_charity_project(
     charity_project: CharityProjectCreate,
     session: AsyncSession = Depends(get_async_session)
 ):
-    await check_name_duplicate(charity_project.name, session)
+    await check_project_name_is_exist(
+        project_name=charity_project.name,
+        session=session
+    )
     new_project = await charity_project_crud.create(charity_project, session)
+    new_project = await investing_process(session, new_project)
     return new_project
+
+
+@router.delete(
+    '/{project_id}',
+    response_model=CharityProjectDB,
+)
+async def delete_charityproject(
+    project_id: int,
+    session: AsyncSession = Depends(get_async_session)
+):
+    project = await check_project_is_exist(project_id, session)
+    project = check_money_amount(project)
+    project = await charity_project_crud.delete(project, session)
+    return project
+
+
+@router.patch(
+    '/{project_id}',
+    response_model=CharityProjectDB,
+)
+async def partially_update_project(
+    project_id: int,
+    obj_in: CharityProjectUpdate,
+    session: AsyncSession = Depends(get_async_session)
+):
+    project = await check_project_is_exist(project_id, session)
+    check_project_is_closed(project)
+    if obj_in.name is not None:
+        await check_project_name_is_exist(
+            project_name=obj_in.name,
+            project_id=project_id,
+            session=session)
+    if obj_in.full_amount is not None:
+        check_money_amount(project, obj_in.full_amount)
+    project = await charity_project_crud.update(obj_in, project, session)
+    return project
--- app/api/validators.py
+++ app/api/validators.py
@@ -4,17 +4,56 @@ from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.charity_project import charity_project_crud
+from app.models import CharityProject
 
 
-async def check_name_duplicate(
+async def check_project_name_is_exist(
+    *,
     project_name: str,
+    project_id: int = None,
     session: AsyncSession
 ) -> None:
-    project = await charity_project_crud.get_project_id_by_name(
+    id = await charity_project_crud.get_project_id_by_name(
         project_name, session
     )
-    if project:
+    if id and id != project_id:
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
-            detail='Проект с таким именем уже создан'
+            detail='Проект с таким именем уже существует'
+        )
+
+async def check_project_is_exist(
+    project_id: int,
+    session: AsyncSession
+) -> CharityProject:
+    project = await charity_project_crud.get(project_id, session)
+    if project is None:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f'Не найден благотворительный проект с id {project_id}'
+        )
+    return project
+
+
+def check_money_amount(obj, new_amount=None):
+    invested = obj.invested_amount
+    if new_amount:
+        if invested > new_amount:
+            raise HTTPException(
+                status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+                detail=f'Сумма не может быть меньше вложенной: {invested}'
+            )
+    elif invested > 0:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Нельзя удалить проект с внесенными инвестициями'
+        )
+    return obj
+
+
+def check_project_is_closed(obj):
+    if obj.fully_invested:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Запрещено редактирование закрытого проекта'
         )
