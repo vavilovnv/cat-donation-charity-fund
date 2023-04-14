4d28cb66a57e03113908db0776f81eeab368c55d
nike <vavilovnv@gmail.com>
2023-04-14 12:09:05 +0300

    feat. standalone app mode added

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,21 @@
 from typing import Optional
 
+from gunicorn.app.wsgiapp import WSGIApplication
 from pydantic import BaseSettings, EmailStr
+
+
+class StandaloneApplication(WSGIApplication):
+    def __init__(self, app_uri, options=None):
+        self.options = options or {}
+        self.app_uri = app_uri
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
 
 
 class Settings(BaseSettings):
@@ -8,6 +23,8 @@ class Settings(BaseSettings):
     app_desc: str = 'Сервис учета пожертвований на котиков'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'any_secret'
+    project_host: str = '127.0.0.1'
+    project_port: int = 8000
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
 
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+import multiprocessing
+
 from fastapi import FastAPI
 
 from app.api.routers import main_router
-from app.core.config import settings
+from app.core.config import StandaloneApplication, settings
 from app.core.init_db import create_first_superuser
 
 app = FastAPI(
@@ -15,3 +17,12 @@ app.include_router(main_router)
 @app.on_event('startup')
 async def startup():
     await create_first_superuser()
+
+
+if __name__ == '__main__':
+    options = {
+        "bind": f'{settings.project_host}:{settings.project_port}',
+        "workers": multiprocessing.cpu_count(),
+        "worker_class": "uvicorn.workers.UvicornWorker",
+    }
+    StandaloneApplication('main:app', options).run()
